99753f25f1c5f8d3073d56a8ca83c31919eae738
ULTRAPAIN <1032210157@tcetmumbai.in>
2025-08-08 22:44:50 +0530

    We created the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-orange-50 via-white to-yellow-50">
+      <Header />
+      <Hero />
+      <Features />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
