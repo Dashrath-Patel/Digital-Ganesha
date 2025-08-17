16a68303f0797c894ec3ae9c05c16970a233607e
ULTRAPAIN <1032210157@tcetmumbai.in>
2025-08-17 22:58:05 +0530

    changes in the features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -82,7 +82,7 @@ const Features = () => {
         </div>
 
         {/* Features Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" role="list">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto" role="list">
           {features.map((feature, index) => (
             <div
               key={index}
